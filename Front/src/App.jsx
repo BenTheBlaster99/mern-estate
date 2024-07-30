18839a0078770a812f174dfa3a08533d8ae083a6
Aimen ouyahia <aymen1889@outlook.fr>
2024-07-30 17:36:08 +0100

    first commit

---
+++ Front/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-blue-600 ">
+        <h1>Wassup</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
